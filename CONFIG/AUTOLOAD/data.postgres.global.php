d41de0fa93404235b6bf8f9522da5b05e194c222
CHGLongStone <jason.medland@gmail.com>
2017-01-17 21:16:30 -0500

    move config stubs

---
+++ CONFIG/AUTOLOAD/data.postgres.global.php
@@ -0,0 +1,23 @@
+<?php 
+/**
+* 
+* 
+* 
+*/
+return array(
+	"DSN" => array(
+		"JCORE_PG" => array(
+			"dbType"=>"POSTGRES",
+			#implementation=>"pg",
+			"host"=>"localhost",
+			"port"=>5432,
+			"database"=>"JCORE",
+			"username"=>"jcore",
+			"password"=>"jcore",
+			"persistent"=>"true",
+		)
+	)
+
+	
+);
+?>
